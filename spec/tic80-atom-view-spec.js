2353000b3176264f8ef28c761595a2834f614238
ViChyavIn <nikita.vyach.ivanov@gmail.com>
2020-08-20 13:49:28 +0500

    Init commit

---
+++ spec/tic80-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Tic80AtomView from '../lib/tic80-atom-view';
+
+describe('Tic80AtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
