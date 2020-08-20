31a757d5d072553902a6bd93ebd480977294ebae
ViChyavIn <nikita.vyach.ivanov@gmail.com>
2020-08-20 20:01:43 +0500

    Tic80View removed

--- spec/tic80-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import Tic80AtomView from '../lib/tic80-atom-view';
-
-describe('Tic80AtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
